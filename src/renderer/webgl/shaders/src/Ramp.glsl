f70608fe0d29fd5483b10e35029726dfef504a22
Ben Richards <benjamindrichards@gmail.com>
2026-02-16 11:20:51 +1300

    Improve SINUSOIDAL Color band interpolation. It's actually sinusoidal now.

--- src/renderer/webgl/shaders/src/Ramp.glsl
+++ src/renderer/webgl/shaders/src/Ramp.glsl
@@ -100,12 +100,8 @@ float interpolate(float progress, int mode)
     }
     if (mode == 2)
     {
-        // SINUSOIDAL or circular: smooth ends, sharp middle.
-        if ((progress *= 2.0) < 1.0)
-        {
-            return -0.5 * (sqrt(1.0 - progress * progress) - 1.0);
-        }
-        return 0.5 * (sqrt(1.0 - (progress -= 2.0) * progress) + 1.0);
+        // SINUSOIDAL or circular: smooth ends, steep middle.
+        return sin((progress - 0.5) * 3.14159265358979323846) * 0.5 + 0.5;
     }
     if (mode == 3)
     {
